3cedef6a5f3a57a959944d3c04cf7b60d27e7305
artem <artem29562@yandex.ru>
2023-02-18 11:48:18 +0300

    task1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        byte age = 17;
+        if (age >= 18) {
+            System.out.println("Вы совершеннолетний");
+
+        } else {
+            System.out.println("Вы не совершеннолетний ");
+        }
+    }
+}
