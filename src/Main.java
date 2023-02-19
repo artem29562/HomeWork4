77f723ad4d094e75b93edaf4ca4def40bfc332f5
artem <artem29562@yandex.ru>
2023-02-19 15:58:03 +0300

    task3

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,29 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task3();
+    }
+        public static void task1() {
+            System.out.println("task 1");
         byte age = 17;
         if (age >= 18) {
-            System.out.println("Вы совершеннолетний");
+            System.out.println("ВамВы совершеннолетний");
 
         } else {
             System.out.println("Вы не совершеннолетний ");
         }
+            System.out.println("===========================");
+    }
+    public static void task3(){
+        System.out.println("task 3");
+        int speed = 60;
+
+        if(speed > 60){
+            System.out.println(" Если скорость больше " + speed +  " придется заплатить штраф");
+        }else {
+            System.out.println( "Eсли скорость меньше или равна " + speed +  " млжно ездить спокойно");
+        }
+
+
     }
 }
